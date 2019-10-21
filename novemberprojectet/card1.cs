731bdd72d83000477d91a75d025c8ab99e2c890f
Hugo <Hugo.wiman@elev.ga.ntig.se>
2019-10-21 11:05:38 +0200

    dag 1
    Kom på vad jag skulle jobba med samt hur jag borde göra programet

--- novemberprojectet/card1.cs
+++ novemberprojectet/card1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace novemberprojectet
 {
-    class Program
+    class card1
     {
-        static void Main(string[] args)
-        {
-            //kortspel. med hjälp av instanser 
-        }
     }
 }
